d25d558ccc193034a4d057ec89d67d41dbe8436c
enigmalea <enigmalea@gmail.com>
2023-01-04 22:40:43 -0500

    [feat[ adds mod page listing - adds new files for mod page and mod component - corrects typos on other pages - removes unneeded default docs

---
+++ src/components/Mods.js
@@ -0,0 +1,158 @@
+import React from "react";
+
+export const Enigmalea = () => {
+	return (
+		<div className="avatar">
+			<img
+				className="avatar__photo avatar__photo-sm"
+				src="/img/mods/enig.png"
+			/>
+			<div className="avatar__intro">
+				<div className="avatar__name">enigmalea</div>
+				<div className="container">
+					<div className="row">
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://www.enigmalea.quest"
+									className="footer__link-item"
+									alt="enigmalea.quest"
+								>
+									<i className="fa-solid fa-house"></i>
+								</a>
+							</small>
+						</div>
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://indiepocalypse.social/@enigmalea"
+									className="footer__link-item"
+									alt="mastodon"
+								>
+									<i className="fa-brands fa-mastodon"></i>
+								</a>
+							</small>
+						</div>
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://enigmalea.tumblr.com"
+									className="footer__link-item"
+									alt="mastodon"
+								>
+									<i className="fa-brands fa-tumblr"></i>
+								</a>
+							</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const FoxInBoots = () => {
+	return (
+		<div className="avatar">
+			<img className="avatar__photo avatar__photo-sm" src="/img/mods/fox.png" />
+			<div className="avatar__intro">
+				<div className="avatar__name">FoxInBoots</div>
+				<div className="container">
+					<div className="row">
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://thefoxinboots.tumblr.com/"
+									className="footer__link-item"
+									alt="tumblr"
+								>
+									<i className="fa-brands fa-tumblr"></i>
+								</a>
+							</small>
+						</div>
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://twitter.com/TheFoxInBoots"
+									className="footer__link-item"
+									alt="twitter"
+								>
+									<i className="fa-brands fa-twitter"></i>
+								</a>
+							</small>
+						</div>
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://mastodon.gamedev.place/@foxinboots"
+									className="footer__link-item"
+									alt="mastodon"
+								>
+									<i className="fa-brands fa-mastodon"></i>
+								</a>
+							</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Dirthenera = () => {
+	return (
+		<div className="avatar">
+			<img
+				className="avatar__photo avatar__photo-sm"
+				src="/img/mods/dirthenera.png"
+			/>
+			<div className="avatar__intro">
+				<div className="avatar__name">Dirthenera</div>
+				<div className="container">
+					<div className="row">
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://dirthenera.tumblr.com/"
+									className="footer__link-item"
+									alt="tumblr"
+								>
+									<i className="fa-brands fa-tumblr"></i>
+								</a>
+							</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Lyfurn = () => {
+	return (
+		<div className="avatar">
+			<img
+				className="avatar__photo avatar__photo-sm"
+				src="/img/mods/lyfurn.png"
+			/>
+			<div className="avatar__intro">
+				<div className="avatar__name">lyfurn</div>
+				<div className="container">
+					<div className="row">
+						<div className="col col--1">
+							<small className="avatar__subtitle">
+								<a
+									href="https://lyfurn.tumblr.com/"
+									className="footer__link-item"
+									alt="tumblr"
+								>
+									<i className="fa-brands fa-tumblr"></i>
+								</a>
+							</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
